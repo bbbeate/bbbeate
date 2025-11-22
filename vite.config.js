a4f2291522c171b81cd59d905bc4f8d8cae6e886
beate-boger <beate.boger@schibsted.com>
2025-11-23 00:06:54 +0100

    configure build for /liste path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bbbeate/',
+  base: '/bbbeate/liste/',
+  build: {
+    outDir: 'dist/liste'
+  }
 })
