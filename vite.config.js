510f01e36b74dcd8144bf96e6476caef0bb73d02
beate-boger <beate.boger@schibsted.com>
2025-11-23 09:21:25 +0100

    add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['beate.png'],
+      manifest: {
+        name: 'liste',
+        short_name: 'liste',
+        description: 'todo list',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'beate.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'beate.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: '/bbbeate/'
 })
