46de86420acae942b7d22d89b0fe65243f8222f3
beate-boger <beate.boger@schibsted.com>
2025-12-07 15:22:26 +0100

    Use shared folder via Vite alias

--- space/vite.config.js
+++ space/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, '../shared')
+    }
+  },
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
