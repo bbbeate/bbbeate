a0961242b4dd934c399abebb1158bc01feeb2f32
beate-boger <beate.boger@schibsted.com>
2026-02-09 20:20:40 +0100

    allow hvaskjer

--- space/vite.config.js
+++ space/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['discoball-icon.png'],
       workbox: {
-        navigateFallbackDenylist: [/^\/spaces/, /^\/liste/, /^\/tunes/]
+        navigateFallbackDenylist: [/^\/spaces/, /^\/liste/, /^\/tunes/, /^\/hvaskjer/]
       },
       manifest: {
         name: 'space',
